fa342ed3af1e50a1a64999c41379bcb06f4008a1
Gary February <gfebruary@gmail.com>
2024-05-31 23:31:42 +0200

    main page looking good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+// import { Routes, Route, NavLink } from "react-router-dom";
 import { createClient } from "contentful";
+import NavBar from "./components/NavBar";
 
 const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
 const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
@@ -11,6 +13,7 @@ const client = createClient({
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -25,6 +28,7 @@ function App() {
         //------------------------------------------------------------------------------------------
 
         setMovies(response.items);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
@@ -33,25 +37,82 @@ function App() {
     fetchMovies();
   }, []);
 
-  const isLoading = movies.length === 0;
+  //----------------------------RANDOM MOVIE FOR BACKGROUND---------------
+  const getRandomMovie = () => {
+    if (movies.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    return movies[randomIndex];
+  };
+
+  //----------------------------RANDOM MOVIES FOR MINI POSTERS---------------
+
+  const getRandomMovies = (count) => {
+    const randomMovies = [];
+    for (let i = 0; i < count; i++) {
+      const randomIndex = Math.floor(Math.random() * movies.length);
+      randomMovies.push(movies[randomIndex]);
+    }
+    return randomMovies;
+  };
+
+  //--------------------------------------------------------------------------
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Loading movies...</p>
-      ) : (
-        <ul>
-          {movies.map((movie, index) => (
-            <li key={index}>
-              <img
-                className="shadow-md rounded-lg w-96"
-                src={movie.fields.poster.fields.file.url}
-              />
-              {movie.fields.title}
-            </li>
+    <div className="position: relative bg-gray-900 min-h-screen ">
+      <NavBar />
+
+      <div className="flex justify-center">
+        {getRandomMovie() && (
+          <img
+            className="self-center h-1/4 w-2/4"
+            src={getRandomMovie().fields.backgroundImage.fields.file.url}
+            alt=""
+          />
+        )}
+      </div>
+
+      <div className="main-selling-point text-center">
+        <p className=" text-white text-4xl ">Track films you’ve watched.</p>
+        <p className="text-white text-4xl">Save those you want to see.</p>
+        <p className="text-white text-4xl">Tell your friends what’s good. </p>
+      </div>
+
+      <div className="flex justify-center">
+        <button className=" bg-green-700 hover:bg-green-800 text-white font-bold p-3 m-9 rounded">
+          Get Started - It's Free
+        </button>
+      </div>
+
+      {!isLoading && (
+        <div className="flex justify-center">
+          {getRandomMovies(6).map((movie, index) => (
+            <img
+              key={index}
+              className="w-40 h-auto m-2 rounded"
+              src={movie.fields.poster.fields.file.url}
+              alt=""
+            />
           ))}
-        </ul>
+        </div>
       )}
+
+      {/* <div className="flex justify-center">
+        {isLoading ? (
+          <p>Loading movies...</p>
+        ) : (
+          <ul>
+            {movies.map((movie, index) => (
+              <li key={index}>
+                <img
+                  className=" shadow-md rounded-lg w-96 self-center m-14 p-14 "
+                  src={movie.fields.poster.fields.file.url}
+                />
+                {movie.fields.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div> */}
     </div>
   );
 }
